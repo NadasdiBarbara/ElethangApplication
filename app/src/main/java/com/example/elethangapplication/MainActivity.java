707302322803834767230da09e89e267a03b6a09
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2022-03-08 17:04:37 +0100

    minden oldal közötti kommunikáció kész

--- app/src/main/java/com/example/elethangapplication/MainActivity.java
+++ app/src/main/java/com/example/elethangapplication/MainActivity.java
@@ -2,21 +2,27 @@ package com.example.elethangapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -25,11 +31,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView navigationView;
     private BottomNavigationView bottomNavigationView;
 
+    private static int SPLASH_TIME_OUT = 5000;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        //spashActiv();
         //hambiicon
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(toggle);
@@ -38,7 +48,54 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNav);
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.exit){
+            AlertDialog.Builder builder = new  AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Biztos kiszeretne lépni?");
+            builder.setCancelable(true);
+
+            builder.setNegativeButton("Igen", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+
+            builder.setPositiveButton("Nem", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+        return true;
+    }
+
+    private void spashActiv() {
+        new Handler(). postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeFragment.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
     public  void  init(){
         toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.drawerLayout);
@@ -51,6 +108,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             switch (item.getItemId()) {
                 case R.id.bottom_kedcencek:
                     replaceFragment(new KedvencekFragment());
+                    break;
+                case R.id.bottom_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.bottom_beallitasok:
+                    replaceFragment(new BeallitasokFragment());
                     break;
             }
             return true;
@@ -66,7 +129,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.dog:
                 replaceFragment(new DogFragment());
                 break;
-
+            case R.id.segitseg:
+                replaceFragment(new HogyanSegithetszFragment());
+                break;
+            case R.id.orokbefogadhatoak:
+                replaceFragment(new OrokbefogadhatoFragment());
+                break;
+            case R.id.virtualis:
+                replaceFragment(new VirtualisFragment());
+                break;
+            case R.id.esemenyek:
+                replaceFragment(new EsemenyekFragment());
+                break;
+            case R.id.programok:
+                replaceFragment(new ProgramokFragment());
+                break;
+            case R.id.rolunk:
+                replaceFragment(new RolunkFragment());
+                break;
+            case R.id.csapatunk:
+                replaceFragment(new CsapatunkFragment());
+                break;
+            case R.id.feltetel:
+                replaceFragment(new FeltetelFragment());
+                break;
         }
 
         drawerLayout.closeDrawer(GravityCompat.START);
@@ -78,4 +164,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.commit();
     }
+
+
 }
