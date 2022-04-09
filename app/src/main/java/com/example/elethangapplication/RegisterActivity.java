c1a41525a091ca1558351ead1fc052885a87be89
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2022-04-09 18:01:20 +0200

    Működő regisztráció és bejelentkezés

--- app/src/main/java/com/example/elethangapplication/RegisterActivity.java
+++ app/src/main/java/com/example/elethangapplication/RegisterActivity.java
@@ -10,15 +10,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText editName,editUsername,editemail,editpsw;
+    private EditText editName;
+    private EditText editUsername;
+    private EditText editemail;
+    private EditText editpsw;
     private Button registerButton;
     private String name, username, email, password;
 
-    private String url = "http://10.148.149.190:8000/api/register";
+    //laptop
+    //private String url = "http://10.148.149.190:8000/api/register";
+    //emulator
+    private String url = "http://10.0.2.2:8000/api/register";
+    //asztali gép
+    //private String url = "http://192.168.0.48:8000/api/register";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,30 +40,12 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                name = editName.getText().toString().trim();
-                username = editUsername.getText().toString().trim();
-                email = editemail.getText().toString();
-                password = editpsw.getText().toString();
-                if (name.isEmpty() ){
-                    Toast.makeText(RegisterActivity.this, "Név megadása kötelező ", Toast.LENGTH_SHORT).show();
-                }else if (name.length() >255){
-                    Toast.makeText(RegisterActivity.this, "Nem lehet több mint 255 karakter", Toast.LENGTH_SHORT).show();
-                }else if (username.isEmpty()){
-                    Toast.makeText(RegisterActivity.this, "Felhasználónév megadása kötelező", Toast.LENGTH_SHORT).show();
-                }else if (username.length() > 255){
-                    Toast.makeText(RegisterActivity.this, "Nem lehet több mint 255 karakter", Toast.LENGTH_SHORT).show();
-                }else if (email.isEmpty()){
-                    Toast.makeText(RegisterActivity.this, "Email megadása kötelező", Toast.LENGTH_SHORT).show();
-                }else if (password.isEmpty()){
-                    Toast.makeText(RegisterActivity.this, "Jelszó megadása kötelező", Toast.LENGTH_SHORT).show();
-                }else if (password.length() <8 || password.length() >30){
-                    Toast.makeText(RegisterActivity.this, "Jelszó minimum 8, maximum 30 karekter", Toast.LENGTH_SHORT).show();
-                }else {
-                    new RequestTask().execute();
-                }
+                RequestTask task  = new RequestTask();
+                task.execute();
             }
         });
     }
+
     public void init(){
         editName = findViewById(R.id.editName);
         editUsername = findViewById(R.id.editRegisterUsername);
@@ -58,20 +53,49 @@ public class RegisterActivity extends AppCompatActivity {
         editpsw = findViewById(R.id.editpsw);
         registerButton = findViewById(R.id.registerButton);
     }
+
     private class RequestTask extends AsyncTask<Void, Void, Response> {
+        private String body;
+
+        @Override
+        protected void onPreExecute() {
+            name = editName.getText().toString().trim();
+            username = editUsername.getText().toString().trim();
+            email = editemail.getText().toString();
+            password = editpsw.getText().toString();
+            if (name.isEmpty() ){
+                Toast.makeText(RegisterActivity.this, "Név megadása kötelező ", Toast.LENGTH_SHORT).show();
+            }else if (name.length() >255){
+                Toast.makeText(RegisterActivity.this, "Nem lehet több mint 255 karakter", Toast.LENGTH_SHORT).show();
+            }else if (username.isEmpty()){
+                Toast.makeText(RegisterActivity.this, "Felhasználónév megadása kötelező", Toast.LENGTH_SHORT).show();
+            }else if (username.length() > 255){
+                Toast.makeText(RegisterActivity.this, "Nem lehet több mint 255 karakter", Toast.LENGTH_SHORT).show();
+            }else if (email.isEmpty()){
+                Toast.makeText(RegisterActivity.this, "Email megadása kötelező", Toast.LENGTH_SHORT).show();
+            }else if (password.isEmpty()){
+                Toast.makeText(RegisterActivity.this, "Jelszó megadása kötelező", Toast.LENGTH_SHORT).show();
+            }else if (password.length() <8 || password.length() >30){
+                Toast.makeText(RegisterActivity.this, "Jelszó minimum 8, maximum 30 karekter", Toast.LENGTH_SHORT).show();
+            }
+            RegisterUser registerUser = new RegisterUser(name, username,email, password);
+            Gson jsonconv = new Gson();
+            body = jsonconv.toJson(registerUser);
+        }
 
         @Override
         protected Response doInBackground(Void... voids) {
 
             Response response = null;
             try {
-                String data = String.format("{\"name\":\"%s\",\"username\":\"%s\",\"email\":\"%s\",\"password\":\"%s\"}",name, username, email, password);
-                response = RequestHandler.post(url, data);
+                response = RequestHandler.post(url, body);
+                Response finalResponse = response;
+                runOnUiThread(()->{
+                    Toast.makeText(RegisterActivity.this, finalResponse.getContent(), Toast.LENGTH_SHORT).show();
+                });
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
             return response;
         }
 
@@ -87,7 +111,6 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-
         }
     }
 }
