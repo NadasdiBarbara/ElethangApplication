be7506fb0ac82fdc964d61a94a08cd83320345ef
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2022-04-09 18:20:42 +0200

    álló mód beállítása, felső információs sáv elrejtése

--- app/src/main/java/com/example/elethangapplication/LoginActivity.java
+++ app/src/main/java/com/example/elethangapplication/LoginActivity.java
@@ -8,6 +8,8 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -38,6 +40,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         sharedPreferences = LoginActivity.this.getSharedPreferences("token", Context.MODE_PRIVATE);
         init();
--- app/src/main/java/com/example/elethangapplication/RegisterActivity.java
+++ app/src/main/java/com/example/elethangapplication/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -35,6 +37,8 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
         init();
         registerButton.setOnClickListener(new View.OnClickListener() {
