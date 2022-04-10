a9649b984c3ca7f96b28f41920852c2e3a6f1016
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2022-04-10 23:18:27 +0200

    kutya örökbefogadás

--- app/src/main/java/com/example/elethangapplication/dog/Dog.java
+++ app/src/main/java/com/example/elethangapplication/dog/Dog.java
@@ -3,14 +3,20 @@ package com.example.elethangapplication.dog;
 import com.google.gson.annotations.SerializedName;
 
 public class Dog {
+    public int id;
     @SerializedName("name")
     public String dogName;
     @SerializedName("description")
     public String dogDescription;
 
-    public Dog(String dogName, String dogDescription) {
+    public Dog(int id, String dogName, String dogDescription) {
+        this.id = id;
         this.dogName = dogName;
         this.dogDescription = dogDescription;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getDogName() {
--- app/src/main/java/com/example/elethangapplication/dog/DogAdapter.java
+++ app/src/main/java/com/example/elethangapplication/dog/DogAdapter.java
@@ -1,24 +1,33 @@
 package com.example.elethangapplication.dog;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.elethangapplication.R;
+import com.example.elethangapplication.RequestHandler;
+import com.example.elethangapplication.Response;
 
+import java.io.IOException;
 import java.util.List;
 
 public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogHolder> {
 
     Context context;
     List<Dog> dogList;
+
+    private String url = "http://10.0.2.2:8000/api/dogAdoptionLoggedin/";
 
     public DogAdapter(Context context, List<Dog> dogList) {
         this.context = context;
@@ -40,11 +49,46 @@ public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogHolder> {
     public void onBindViewHolder(@NonNull DogAdapter.DogHolder holder, int position) {
         holder.dogName.setText(dogList.get(position).getDogName());
         holder.dogDescription.setText(dogList.get(position).getDogDescription());
+        Dog dog = dogList.get(position);
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        holder.btnDogAdoption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "Kattaintás "+holder.getAdapterPosition(), Toast.LENGTH_SHORT).show();
+                SharedPreferences sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
+                AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                alert.setPositiveButton("Virtuális örökbefogadás", (dialogInterface, i) -> {
+                    AsyncTask<Void, Void, Response> task = new AsyncTask<Void, Void, Response>(){
+                        @Override
+                        protected Response doInBackground(Void... voids) {
+                            Response response = null;
+                            try {
+                                response = RequestHandler.postAuth(url+ dog.id,"{\"adoption_type_id\":2}",sharedPreferences.getString("token",""));
+                                Log.d("Response", response.getContent());
+                            } catch (IOException e) {
+                                Log.d("Response error", e.getMessage());
+                            }
+                            return response;
+                        }
+                    };
+                    task.execute();
+                });
+                alert.setNegativeButton("Általános Örökbefogadás", (dialogInterface, i) -> {
+                    AsyncTask<Void, Void, Response> task = new AsyncTask<Void, Void, Response>(){
+                        @Override
+                        protected Response doInBackground(Void... voids) {
+                            Response response = null;
+                            try {
+                                response = RequestHandler.postAuth(url+ dog.id,"{\"adoption_type_id\":1}",sharedPreferences.getString("token",""));
+                                Log.d("Response", response.getContent());
+                            } catch (IOException e) {
+                                Log.d("Response error", e.getMessage());
+                            }
+                            return response;
+                        }
+                    };
+                    task.execute();
+                });
+                alert.create().show();
             }
         });
     }
@@ -59,11 +103,13 @@ public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogHolder> {
         TextView dogName;
         TextView dogDescription;
         ImageView dogimage;
+        Button btnDogAdoption;
         public DogHolder(@NonNull View itemView) {
             super(itemView);
             dogName = itemView.findViewById(R.id.dogName);
             dogDescription = itemView.findViewById(R.id.dogDescription);
             dogimage = itemView.findViewById(R.id.pictureDog);
+            btnDogAdoption = itemView.findViewById(R.id.btnDogadoption);
         }
     }
 }
