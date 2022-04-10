164f2eb5e8293efd50c865363b94b0768b0dbf79
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2022-04-10 22:46:28 +0200

    programjelentkezés elkezdése

--- app/src/main/java/com/example/elethangapplication/program/ProgramAdapter.java
+++ app/src/main/java/com/example/elethangapplication/program/ProgramAdapter.java
@@ -1,18 +1,25 @@
 package com.example.elethangapplication.program;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.elethangapplication.R;
+import com.example.elethangapplication.RequestHandler;
+import com.example.elethangapplication.Response;
 import com.example.elethangapplication.cat.CatAdapter;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramHolder> {
@@ -20,7 +27,7 @@ public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramH
     Context context;
     List<Program>programList;
 
-
+    private String url = "http://10.0.2.2:8000/api/storeProgramApplication";
 
     public ProgramAdapter(Context context, List<Program> programList) {
         this.context = context;
@@ -44,6 +51,16 @@ public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramH
         holder.programType.setText(programList.get(position).getType());
         holder.date.setText(programList.get(position).getDate());
         holder.time.setText(programList.get(position).getTime());
+        
+        Program program = programList.get(position);
+        
+        holder.btnjelentkezik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
+
+            }
+        });
     }
 
     @Override
