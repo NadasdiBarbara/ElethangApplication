503b6800ff3c9f55d6b017d24b6d1158b7d1b568
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2022-04-23 16:13:09 +0200

    programjelentkezés működik

--- app/src/main/java/com/example/elethangapplication/program/ProgramAdapter.java
+++ app/src/main/java/com/example/elethangapplication/program/ProgramAdapter.java
@@ -1,5 +1,6 @@
 package com.example.elethangapplication.program;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -12,12 +13,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.elethangapplication.R;
 import com.example.elethangapplication.RequestHandler;
 import com.example.elethangapplication.Response;
 import com.example.elethangapplication.cat.CatAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
@@ -26,6 +31,7 @@ public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramH
 
     Context context;
     List<Program>programList;
+    Program program;
 
     private String url = "http://10.0.2.2:8000/api/storeProgramApplication";
 
@@ -47,18 +53,29 @@ public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramH
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ProgramAdapter.ProgramHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ProgramAdapter.ProgramHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.programType.setText(programList.get(position).getType());
         holder.date.setText(programList.get(position).getDate());
         holder.time.setText(programList.get(position).getTime());
         
-        Program program = programList.get(position);
+
         
         holder.btnjelentkezik.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
 
+                AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                alert.setMessage("Biztos szeretne jelentkezni?");
+                alert.setPositiveButton("Igen", (dialogInterface, i) -> {
+                    program = programList.get(position);
+                    RequestTask rt =new RequestTask();
+                    rt.execute();
+                });
+                alert.setNegativeButton("Nem", (dialogInterface, i) -> {
+                    dialogInterface.cancel();
+                });
+                AlertDialog alertDialog = alert.create();
+                alertDialog.show();
             }
         });
     }
@@ -67,6 +84,37 @@ public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramH
     public int getItemCount() {
         return programList.size();
     }
+
+    private class RequestTask extends AsyncTask<Void, Void, Response>{
+
+        @Override
+        protected Response doInBackground(Void... voids) {
+
+            Response response = null;
+
+            SharedPreferences sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
+
+            try {
+                    String program_info_id = "{\"program_info_id\":"+program.getId()+"}";
+                    response = RequestHandler.postAuth(url,program_info_id, sharedPreferences.getString("token",""));
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(Response response) {
+            super.onPostExecute(response);
+            if (response.getResponseCode()<400){
+                Toast.makeText(context.getApplicationContext(), "Sikeres jelentkezés", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
     public static class ProgramHolder extends RecyclerView.ViewHolder{
         TextView programType;
         TextView date;
