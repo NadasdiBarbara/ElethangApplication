d3daf668b43f39091b80bfe2a9ebbdeacf74d345
Nadasdi Barbara <nadasdibarbara15@gmail.com>
2022-04-24 12:43:38 +0200

    adattagok rendezvények osztály

--- app/src/main/java/com/example/elethangapplication/events/Event.java
+++ app/src/main/java/com/example/elethangapplication/events/Event.java
@@ -2,17 +2,13 @@ package com.example.elethangapplication.events;
 
 import com.google.gson.annotations.SerializedName;
 
-
-
 public class Event {
-
     @SerializedName("event_name")
-    public String eventName;
+    private String eventName;
     @SerializedName("description")
-    public String eventDescription;
+    private String eventDescription;
     @SerializedName("date")
-    public String eventDate;
-
+    private String eventDate;
 
     public Event(String event_name, String eventDate, String eventDescription) {
         this.eventName = event_name;
@@ -22,23 +18,18 @@ public class Event {
     public String getEventDate() {
         return eventDate;
     }
-
     public void setEventDate(String eventDate) {
         this.eventDate = eventDate;
     }
     public String getEvent_name() {
         return eventName;
     }
-
     public void setEvent_name(String event_name) {
         this.eventName = event_name;
     }
-
-
     public String getDescription() {
         return eventDescription;
     }
-
     public void setDescription(String description) {
         this.eventDescription = description;
     }
